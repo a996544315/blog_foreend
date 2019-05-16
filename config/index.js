024b53159e9abaf2d3558770e2bc929024ce2a14
zhy <996544315@qq.com>
2019-05-16 12:14:43 +0800

    v1.0.0 将端口默认为80

--- config/index.js
+++ config/index.js
@@ -23,13 +23,13 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8082,
+    port: 80,
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
         '/api': {
-            target: 'http://localhost:8080',
+            target: 'http://localhost:9080',
             changeOrigin: true,
             pathRewrite: {
                 '^/api': ''
